ea002ed67f68e1b3ff2bdd92498d39a3d2c636c8
huanggungyao <huangugangyao@rd.com>
2016-08-26 16:22:45 +0800

    addconfigFile

--- common.js
+++ common.js
@@ -1,7 +1,9 @@
 var glob=require("glob");
+var fs=require("fs");
 // console.log(process)
 // var doudouPath= "e:/projectA/source/doudou/WebRoot/play/"
-var doudouPath= "E:/projectA/source/doudou/WebRoot/play/"
+var config=JSON.parse(fs.readFileSync("./config.json"));
+var doudouPath= config.gameRoot;
 module.exports.getAllGame=function(){
     return glob.sync(doudouPath+"*").filter(function(ipath){
             return /^\d+-\w+(?!\.\w+)$/.test(ipath.split(doudouPath)[1]);
